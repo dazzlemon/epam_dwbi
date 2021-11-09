db90f82ae091218a996be99b7681226dbb6b2e2b
dazzlemon <daanill13470@gmail.com>
2021-11-09 18:38:23 +0200

    populate done

--- hw1/hw.sql
+++ hw1/hw.sql
@@ -21,6 +21,8 @@
 -- - 9. Create Diagram of your database schema. It is an optional task. You can create a diagram using SQL Server Management Studio if you use SQL Server. Or you can draw one.
 -- - 10. Upload your homework (2 .sql files and picture of diagram of your DB Schema) to your Homework channel, SQL Basics folder.
 
+-- Create
+
 CREATE DATABASE School;
 USE School;
 
@@ -63,16 +65,17 @@ CREATE TABLE Class( Id            INT IDENTITY(1, 1)
 
 CREATE TABLE Schedule( [Day]     SMALLINT NOT NULL
                      , [Time]    TIME     NOT NULL
-                     , IsOddWeek BIT      NOT NULL
+                     , IsOddWeek BIT -- if NULL same for both weeks
                      , IsLecture BIT      NOT NULL
                      , ClassId   INT      NOT NULL
                      , GroupId   INT      NOT NULL
                      
                      , CONSTRAINT FK_Schedule_ClassId FOREIGN KEY (ClassId) REFERENCES Class   (Id)
-                     , CONSTRAINT FK_Schedule_GroupId FOREIGN KEY (GroupId) REFERENCES [Group] (Id));
+                     , CONSTRAINT FK_Schedule_GroupId FOREIGN KEY (GroupId) REFERENCES [Group] (Id)
+                     , CONSTRAINT CHK_Schedule CHECK ([Day] BETWEEN 0 AND 6)); -- 0 is monday
 
 
-CREATE TABLE Student( Id                INT
+CREATE TABLE Student( Id                INT  IDENTITY(1, 1)
                     , FirstName         TEXT NOT NULL
                     , LastName          TEXT NOT NULL
                     , IsInFirstSubgroup BIT  NOT NULL -- groups are divided into two for labs
@@ -80,3 +83,41 @@ CREATE TABLE Student( Id                INT
                     
                     , CONSTRAINT PK_Student         PRIMARY KEY (Id)
                     , CONSTRAINT FK_Student_GroupId FOREIGN KEY (GroupId) REFERENCES [Group] (Id));
+
+-- Populate
+
+INSERT INTO Room (Id )
+          VALUES (202)
+               , (303)
+               , (404)
+               , (504)
+               , (323)
+               , (220);
+
+INSERT INTO [Group] (Id )
+             VALUES (911)
+                  , (912)
+                  , (931)
+                  , (940);
+
+INSERT INTO Teacher (FirstName, LastName  )
+             VALUES ('John',    'Nash'    )
+                  , ('Terence', 'Tao'     )
+                  , ('fname1',  'fname2'  )
+                  , ('another', 'cool guy')
+                  , ('and one', 'more'    );
+
+INSERT INTO Class (                Name,          LecturerId, InstructorId1, LectureRoomId, RoomId1)
+           VALUES (       'Game Theory',          1,             3,           504,     323) -- not good idea using raw fk but whatever
+                , ('Probability Theory',          2,             4,           404,     303)
+                , (  'One more subject',          3,             4,           220,     303);
+
+INSERT INTO Schedule ([Day],  [Time], IsOddWeek, IsLecture, ClassId, GroupId)
+              VALUES (    0,  '9:30',         0,         1,       1,     911)
+                   , (    0,  '9:30',         1,         1,       2,     911)
+                   , (    0, '11:00',      NULL,         0,       3,     911);
+
+INSERT INTO Student (FirstName,  LastName, IsInFirstSubgroup, GroupId)
+             VALUES ( 'Daniel', 'Safonov',                 0,     911)
+                  , (   'John',    'Wick',                 1,     911);
+
